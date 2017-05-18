7917e398f7d0bfba184885cdaf8e6c57c2f44b23
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-18 12:07:12 -0400

    Lambton Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -1,82 +1,3 @@
-function init() {
-    var header_stores = getStoresList();
-    renderStoreList('#brand_select','#brand_select_template', header_stores, "stores");
-    $("#brand_select").prepend("<option disabled selected>Brands</option>");
-    
-    $("#brand_select").on('change', function() {            
-        if ($(this).val() != ""){
-            window.location = "/stores/"+ $(this).val();    
-        }
-    });  
-
-    // $("#brand_select").bind("change", function(e) {
-    //     if ($("#brand_select option:selected").val() != ""){
-    //         window.location = "/stores/"+ $("#brand_select option:selected").val();    
-    //     }
-    // });  
-
-    renderHomeHours();
-    
-    var prop_details = getPropertyDetails();
-    renderPropertyDetails('#prop_phone_container', '#prop_phone_template', prop_details);
-    
-    var feature_items = getFeatureList();
-    var one_item = feature_items.slice(0,1);
-    renderFeatureItems('#feature_item','#feature_item_template', one_item);
-    var two_items = feature_items.slice(1,3);
-    renderFeatureItems('#home_feature','#home_feature_template', two_items);
-    
-    var _fbq = window._fbq || (window._fbq = []);
-    if (!_fbq.loaded) {
-        var fbds = document.createElement('script');
-        fbds.async = true;
-        fbds.src = '//connect.facebook.net/en_US/fbds.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(fbds, s);
-        _fbq.loaded = true;
-      }
-    _fbq.push(['addPixelId', '548352815262916']);
-    window._fbq = window._fbq || [];
-    window._fbq.push(['track', 'PixelInitialized', {}]);
-    
-    $(".long_feature_box").hover(function() {
-        $(this).find(".long_feature_label").animate({
-            "top": "-=81%"
-        }, 500)
-    }, function() {
-        $(this).find(".long_feature_label").animate({
-            "top": "+=81%"
-        }, 500)
-    });
-    
-    //Campaign Monitor Sign Up
-    $('#subForm').submit(function (e) {
-        if ($("#agree_terms").prop("checked") != true){
-            alert("Please agree to the term and conditions.");
-            $("#agree_terms").focus();
-            return false;
-        }
-        e.preventDefault();
-        $.getJSON(
-            this.action + "?callback=?",
-            $(this).serialize(),
-            function (data) {
-                if (data.Status === 400) {
-                    alert("Please try again later.");
-                } else { // 200
-                    $('#subForm').trigger('reset');
-                    $("#success_subscribe").fadeIn();
-                    
-                    setTimeout(function(){ 
-                        $(".modal-backdrop").remove();
-	                    $(".popup_home").remove();
-                    }, 2000);
-                }
-        });
-    });
-}
-
-      
 function renderGeneral(container, template, collection){
     var item_list = [];
     var item_rendered = [];
@@ -88,6 +9,7 @@ function renderGeneral(container, template, collection){
     });
     $(container).html(item_rendered.join(''));
 }
+
 function renderPropertyDetails(container, template, collection){
     var item_list = [];
     var item_rendered = [];
@@ -99,6 +21,7 @@ function renderPropertyDetails(container, template, collection){
 
     $(container).html(item_rendered.join(''));
 }
+
 function renderBanner(home_banner_template, home_banner, banners){
     var item_list = [];
     var item_rendered = [];
