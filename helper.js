3a1b171e62d1ecfeff41fbad0f170a9afe772b5c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-18 12:57:06 -0400

    Lambton Mall 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -6,22 +6,7 @@ function init() {
             window.location = "/stores/"+ $(this).val();    
         }
     });  
-    
-    var header_stores = getStoresList();
-    renderStoreList('#brand_select','#brand_select_template', header_stores, "stores");
-    $("#brand_select").prepend("<option disabled selected>Brands</option>");
-    
-    renderHomeHours();
-    
-    var prop_details = getPropertyDetails();
-    renderPropertyDetails('#prop_phone_container', '#prop_phone_template', prop_details);
-    
-    var feature_items = getFeatureList();
-    var one_item = feature_items.slice(0,1);
-    renderFeatureItems('#feature_item','#feature_item_template', one_item);
-    var two_items = feature_items.slice(1,3);
-    renderFeatureItems('#home_feature','#home_feature_template', two_items);
-    
+
     var _fbq = window._fbq || (window._fbq = []);
     if (!_fbq.loaded) {
         var fbds = document.createElement('script');
@@ -75,4 +60,19 @@ function init() {
 function show_content(){
     $(".loading_backdrop").remove();
     $('.yield').fadeIn();
+    
+    var header_stores = getStoresList();
+    renderStoreList('#brand_select','#brand_select_template', header_stores, "stores");
+    $("#brand_select").prepend("<option disabled selected>Brands</option>");
+    
+    renderHomeHours();
+    
+    var prop_details = getPropertyDetails();
+    renderPropertyDetails('#prop_phone_container', '#prop_phone_template', prop_details);
+    
+    var feature_items = getFeatureList();
+    var one_item = feature_items.slice(0,1);
+    renderFeatureItems('#feature_item','#feature_item_template', one_item);
+    var two_items = feature_items.slice(1,3);
+    renderFeatureItems('#home_feature','#home_feature_template', two_items);
 }
