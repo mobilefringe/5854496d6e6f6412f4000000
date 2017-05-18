a8acb92d5906f7fa2003e2b6e13f3711996f8a16
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-18 12:19:34 -0400

    Lambton Mall 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,5 +1,5 @@
 function init() {
-    $('<div class="modal-backdrop"><div class="loader">Loading...</div></div>').appendTo(document.body);
+    $('<div class="modal-backdrop loader-backdrop"><div class="loader">Loading...</div></div>').appendTo(document.body);
     
     $("#brand_select").on('change', function() {            
         if ($(this).val() != ""){
