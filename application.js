ed496c4e3469b1e6dd21866517933e2a1f55a8e5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-12 12:35:33 -0400

    Lambton Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -248,7 +248,7 @@ function renderEvents(container, template, collection){
             val.store_name = "Lambton Mall";
         }
         
-        if(val.event_image_url_abs.indexOf('missing.png') > 0){
+        if(val.event_image_url_abs.indexOf('missing.png') < 0){
             val.event_image_url_abs = val.logo;
         }
         // else{
