abd49f2b19637112d7b874e6797cb64f512425d0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-11 12:35:04 -0500

    Lambton Mall 1.1  /helper.js

--- helper.js
+++ helper.js
@@ -65,8 +65,8 @@ function init(e) {
     });
     
     //dynamically changing copyright year
-var current_year = moment().year();
-$("#current_year").text(current_year);
+    var current_year = moment().year();
+    $("#current_year").text(current_year);
 
 }
 
